f56e8b57f576dedfe73817cbc965098c61e2ec85
nurali-code <nur3.dav.97@gmail.com>
2023-11-13 17:05:10 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.hero-slider').slick({
+        arrows: true,
+        infinite: true,
+        speed: 600,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
+        slidesToShow: 1,
+        centerPadding: '0px',
+        centerMode: true,
+    });
+
+});
+
