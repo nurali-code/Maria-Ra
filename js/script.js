573c66076ea560973fb58453b2b1b9bbdfc41ea6
nurali-code <nur3.dav.97@gmail.com>
2023-11-14 15:34:30 +0500

    menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top, }, 300,)
+    });
+
+    $('.btn-menu').on('click', function (e) {
+        $(this).toggleClass('active');
+        $('.header .nav, body').toggleClass('active');
+    });
+
     $('.hero-slider').slick({
         arrows: true,
         infinite: true,
@@ -26,6 +36,42 @@ $(document).ready(function () {
             },
         ]
     });
+
+    function hideModals() {
+        $('.modal').fadeOut();
+        $('body, .header .nav, .btn-menu').removeClass('active');
+    };
+
+    $(function () {
+        function showModal(id) {
+            $(id).fadeIn(300);
+            $('body').addClass('active');
+        }
+
+        $('[data-modal]').on('click', function (e) {
+            e.preventDefault();
+            showModal('#' + $(this).attr("data-modal"));
+        });
+
+        $('.modal-close').on('click', () => {
+            hideModals();
+        });
+
+        $(document).on('click', function (e) {
+            if (!(
+                ($(e.target).parents('.modal-content').length) ||
+                // ($(e.target).parents('.nav').length) ||
+                // ($(e.target).hasClass('nav')) ||
+                ($(e.target).hasClass('btnModal')) ||
+                ($(e.target).hasClass('btn')) ||
+                ($(e.target).hasClass('btn-menu')) ||
+                ($(e.target).hasClass('modal-content'))
+            )) {
+                hideModals();
+            }
+        });
+    });
+
 
     $('.dropdown-btn').on('click', function (e) {
         if ($(this).hasClass('active')) { $('.dropdown-btn').removeClass('active').next().slideUp(); }
